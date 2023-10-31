44c223ab7aa79660efbbe5f220e022ac6411f403
root <dominikgadomski654@gmail.com>
2023-10-31 13:21:28 +0100

    test

--- html/cognito.js
+++ html/cognito.js
@@ -1,6 +1,6 @@
     AWS.config.region = 'eu-central-1'; // Your AWS region
     AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-        IdentityPoolId: 'eu-central-1:218203ec-a4cc-49cb-b3fa-e36d3226c40e	' // Your Cognito Identity Pool ID
+        IdentityPoolId: 'eu-central-1:218203ec-a4cc-49cb-b3fa-e36d3226c40e' // Your Cognito Identity Pool ID
     });
     var poolData = {
         UserPoolId : 'eu-central-1_zYFiuQmA7', // Your Cognito User Pool ID
