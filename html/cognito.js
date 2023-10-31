f36a58fc43b03badb90e0614fa185f07f32804cd
root <dominikgadomski654@gmail.com>
2023-10-31 13:16:25 +0100

    testing cognito

--- html/cognito.js
+++ html/cognito.js
@@ -1,5 +1,7 @@
     AWS.config.region = 'eu-central-1'; // Your AWS region
-
+    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+        IdentityPoolId: 'eu-central-1:218203ec-a4cc-49cb-b3fa-e36d3226c40e	' // Your Cognito Identity Pool ID
+    });
     var poolData = {
         UserPoolId : 'eu-central-1_zYFiuQmA7', // Your Cognito User Pool ID
         ClientId : '276ffcfl48t6id880f4n4najqq' // Your Cognito App Client ID
