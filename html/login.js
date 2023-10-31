839dff23cd3e2720779ba38753c991af69073622
root <dominikgadomski654@gmail.com>
2023-10-31 14:18:25 +0100

    testing login page

---
+++ html/login.js
@@ -0,0 +1,36 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevents the default form submission
+
+    var username = document.getElementById('loginUsername').value;
+    var password = document.getElementById('loginPassword').value;
+
+    var authenticationData = {
+        Username: username,
+        Password: password,
+    };
+    var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
+
+    var userData = {
+        Username: username,
+        Pool: userPool
+    };
+    var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+
+    cognitoUser.authenticateUser(authenticationDetails, {
+        onSuccess: function(result) {
+            // User authentication was successful
+            console.log('Login successful!');
+            // You can now use the Cognito tokens, redirect the user, etc.
+        },
+
+        onFailure: function(err) {
+            // Handle errors
+            console.error(err.message || JSON.stringify(err));
+            alert("Błąd logowania: " + err.message);
+        },
+
+        // Optional: Implement MFA, newPasswordRequired, etc.
+        // These can be added as additional callback methods
+    });
+});
+
