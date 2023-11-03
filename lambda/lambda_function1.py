a08a0bcb6a7a75493693c75aed521c411f5c8913
root <dominikgadomski654@gmail.com>
2023-11-03 21:08:03 +0100

    starting branch static

--- lambda/lambda_function1.py
+++
@@ -1,9 +0,0 @@
-def lambda_handler(event, context):
-    # Wydrukuj "Hello, World!" do dziennika CloudWatch
-    print("Hello, World!")
-    
-    # Zwróć "Hello, World!" jako odpowiedź
-    return {
-        'statusCode': 200,
-        'body': 'Hello, World!'
-    }
