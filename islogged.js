85aad28d7d8fa5c5dc0a4b21cba2008138857d10
Gadom <69717399+Gadom654@users.noreply.github.com>
2023-11-04 17:02:09 +0100

    Update islogged.js

--- islogged.js
+++ islogged.js
@@ -1,19 +1,1 @@
 var cognitoUser = userPool.getCurrentUser();
-
-if (cognitoUser != null) {
-    cognitoUser.getSession(function(err, session) {
-        if (err) {
-            console.error(err);
-            // Redirect back to login if there is an error
-            window.location.href = 'login.html';
-        } else if (!session.isValid()) {
-            console.log("Session is invalid.");
-            // Redirect to login if session is invalid
-            window.location.href = 'login.html';
-        }
-    });
-} else {
-    // If cognitoUser is null, redirect to login
-    window.location.href = 'login.html';
-}
-
