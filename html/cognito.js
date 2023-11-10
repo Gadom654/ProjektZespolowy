682d47f062542d4adcb769309a6a001e765c8eb4
root <dominikgadomski654@gmail.com>
2023-11-10 15:40:54 +0100

    fixing cognito settings

--- html/cognito.js
+++ html/cognito.js
@@ -1,9 +1,9 @@
     AWS.config.region = 'eu-central-1'; // Your AWS region
     AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-        IdentityPoolId: 'eu-central-1:218203ec-a4cc-49cb-b3fa-e36d3226c40e' // Your Cognito Identity Pool ID
+        IdentityPoolId: 'eu-central-1:6c5e9f54-7c9b-4899-be1d-39b35dba174b' // Your Cognito Identity Pool ID
     });
     var poolData = {
-        UserPoolId : 'eu-central-1_zYFiuQmA7', // Your Cognito User Pool ID
-        ClientId : '276ffcfl48t6id880f4n4najqq' // Your Cognito App Client ID
+        UserPoolId : 'eu-central-1_uDcvU5Q1v', // Your Cognito User Pool ID
+        ClientId : '1cfmf8g3m2ivat7q6rdc4rcdgm' // Your Cognito App Client ID
     };
     var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
