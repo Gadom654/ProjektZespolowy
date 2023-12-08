195c7f440dca9e3418e043f3bad9781418e910e0
Gadom <69717399+Gadom654@users.noreply.github.com>
2023-12-08 14:53:57 +0100

    Lambdacorrections (#16)
    * working on backend using lambda
    
    * lambda fixing
    
    * fixing
    
    ---------
    
    Co-authored-by: root <root@LAPTOP-8P6GJ9RR.localdomain>
    Co-authored-by: gadom654 <gadom@LAPTOP-8P6GJ9RR.localdomain>

--- html/login.js
+++ html/login.js
@@ -1,4 +1,4 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
+document.getElementById('loginForm').addEventListener('submit', function (event) {
     event.preventDefault(); // Prevents the default form submission
 
     var username = document.getElementById('loginUsername').value;
@@ -16,30 +16,30 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     };
     var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
 
-cognitoUser.authenticateUser(authenticationDetails, {
-    onSuccess: function(result) {
-        console.log('Login successful!');
+    cognitoUser.authenticateUser(authenticationDetails, {
+        onSuccess: function (result) {
+            console.log('Login successful!');
 
-        // Replace Lambda invoke with Fetch API request to API Gateway
-        fetch('https://xrb4ovumaj.execute-api.eu-central-1.amazonaws.com/getter', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ userId: username })
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log("API Gateway response:", data);
-            window.location.href = data.url;
-        })
-        .catch(err => {
-            console.error('API Gateway error:', err);
-        });
-    },
-    onFailure: function(err) {
-        console.error(err.message || JSON.stringify(err));
-        alert("Błąd logowania: " + err.message);
-    },
-});
-
+            // Replace Lambda invoke with Fetch API request to API Gateway
+            fetch('https://xrb4ovumaj.execute-api.eu-central-1.amazonaws.com/getter', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ userId: username })
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log("API Gateway response:", data);
+                    window.location.href = data.url;
+                })
+                .catch(err => {
+                    console.error('API Gateway error:', err);
+                });
+        },
+        onFailure: function (err) {
+            console.error(err.message || JSON.stringify(err));
+            alert("Błąd logowania: " + err.message);
+        },
+    });
+})
