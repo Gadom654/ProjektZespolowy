7a1dab1eaf780b9afa03c8557fd144f700d7e3ae
root <root@LAPTOP-8P6GJ9RR.localdomain>
2023-12-08 13:07:53 +0100

    working on backend using lambda

--- html/login.js
+++ html/login.js
@@ -20,11 +20,24 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
         onSuccess: function(result) {
             // User authentication was successful
             console.log('Login successful!');
-            // You can now use the Cognito tokens, redirect the user, etc.
-	     window.location.href = 'update.html';
-
-        },
-
+            var AWS = require('aws-sdk');
+            AWS.config.region = 'eu-central-1';
+	    	var lambda = new AWS.Lambda();
+	
+	    	var params = {
+	        	FunctionName: "getter",
+				Payload: JSON.stringify({userId: Username})
+	    	};
+	    	lambda.invoke(params, function(err, data) {
+				if  (err) {
+		   		 	console.log(err, err.stack);
+				} else {
+		    		console.log("Lambda function executed:", data);
+					var resposneData = JSON.parse(data.Payload);
+					window.location.href = responseData.url;
+        		}
+				});
+		},
         onFailure: function(err) {
             // Handle errors
             console.error(err.message || JSON.stringify(err));
--- lambda/getter.py
+++ lambda/getter.py
@@ -1,27 +1,29 @@
-import json
-import boto3
-from botocore.exceptions import ClientError
-
-# Initialize a DynamoDB client
-dynamodb = boto3.resource('dynamodb')
+mport boto3
 
 def lambda_handler(event, context):
-    # Replace 'YourTableName' with your actual DynamoDB table name
-    table = dynamodb.Table('usersdata')
+    dynamodb = boto3.resource('dynamodb')
+    user_table = dynamodb.Table('usersdata')
+    alergy_table = dynamodb.Table('alergydata')
+    
+    username = event['userId']  # Assuming userId is passed in the event
 
-    try:
-        # Replace 'userId' with the actual key column name and 'event['userId']' with the key value passed from API Gateway
-        response = table.get_item(Key={'user': event['user']})
-    except ClientError as e:
-        print(e.response['Error']['Message'])
-        return {
-            'statusCode': 500,
-            'body': json.dumps("Error in retrieving item")
-        }
-    else:
-        item = response.get('Item', {})
-        return {
-            'statusCode': 200,
-            'body': json.dumps(item)
-        }
+    # Check if user has data in usersdata table
+    user_response = user_table.get_item(Key={'username': username})
+    if 'Item' not in user_response:
+        return {'url': 'update.html'}
+
+    # Check the alergydata table for brzoza value
+    alergy_response = alergy_table.get_item(Key={'alergy': 'brzoza'})
+    brzoza_value = int(alergy_response['Item']['poziompy'])
+
+    # Determine which page to return based on brzoza value and user preference
+    if user_response['Item'].get('brzoza') == 'yes':
+        if brzoza_value <= 2:
+            return {'url': '1.html'}
+        elif brzoza_value > 4:
+            return {'url': '3.html'}
+        else:
+            return {'url': '2.html'}
+
+    return {'url': 'default.html'}  # Default response if conditions are not met
 
